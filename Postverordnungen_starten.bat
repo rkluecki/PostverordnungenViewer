112b0517927a637ef200106fdd94dd398b999e0a
rkluecki <164741964+rkluecki@users.noreply.github.com>
2026-06-03 15:35:33 +0200

    Ordinata Startskript auf Suite-Ordner umgestellt

--- Postverordnungen_starten.bat
+++ Postverordnungen_starten.bat
@@ -1,5 +1,5 @@
 @echo off
-cd /d "D:\Postgeschichte_PC\IT-Entwicklung\Javaprojekt\Postverordnungen"
+cd /d "D:\Postgeschichte_PC\IT-Entwicklung\Historische_Suite_Projektordner\Ordinata\app\Postverordnungen"
 
 "D:\Program Files\JetBrains\IntelliJ IDEA 2025.3.3\plugins\maven\lib\maven3\bin\mvn.cmd" javafx:run
 
